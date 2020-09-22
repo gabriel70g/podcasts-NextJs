18aaf45200bcb3ea1bf1eb46c0c14ca7d5ab4569
Gabriel Gomez <gabriel70@gmail.com>
2020-09-21 22:47:17 -0300

    Primer commit

---
+++ components/Layout.js
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import Head from 'next/head';
+
+export default class Layout extends React.Component {
+  render() {
+    const { children, title } = this.props
+
+    return (
+      <>
+        <Head>
+          <title>{title}</title>
+        </Head>
+        <header><Link href="/"><a> Podcasts</a></Link></header>
+        { children}
+        <style jsx>{`
+        header {
+          color: #fff;
+          background: #8756ca;
+          padding: 15px;
+          text-align: center;
+          
+        }
+        header a {
+          color: #fff;
+          text-decoration: none;
+        }
+    `}</style>
+        <style jsx global>{`
+        body {
+          margin: 0;
+          font-family: system-ui;
+          background: white;
+        }
+      `}</style>
+
+      </>
+    )
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import 'isomorphic-fetch';
+import Layout from '../components/Layout';
+import ChannelGrid from '../components/ChannelGrid';
+
+export default class extends React.Component {
+
+  static async getInitialProps() {
+    let req = await fetch('https://api.audioboom.com/channels/recommended')
+    let { body: channels } = await req.json()
+    return { channels }
+  }
+
+  render() {
+    const { channels } = this.props
+
+    return (
+      <>
+        <Layout title="App de podcasts">
+          <ChannelGrid channels={ channels } />
+        </Layout>
+      </>
+    )
+  }
+}
